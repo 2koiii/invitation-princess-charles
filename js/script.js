4941afe8da257304cc7af014b6b629fd0c183cd5
Mox <cjrmox@gmail.com>
2023-06-17 11:31:04 +0800

    updated everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
-const overlay = document.querySelector('.overlay');
+// const overlay = document.querySelector('.overlay');
 
-overlay.addEventListener('click', () => {
-    overlay.classList.toggle('overlay-hidden');
+// overlay.addEventListener('click', () => {
+//     overlay.classList.toggle('overlay-hidden');
+// });
+
+// For animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show'); // is you only want animation to show once you can stop here
+            entry.target.classList.add('stagger-show'); // is you only want animation to show once you can stop here
+        } else {
+            entry.target.classList.remove('show'); // add this if you van to reboot the class when it's not intersecting
+            entry.target.classList.remove('stagger-show'); // add this if you van to reboot the class when it's not intersecting
+        }
+    });
+});
+
+// close window onclick
+function close_window() {
+    if (confirm("Close Window?")) {
+      close();
+    }
+  }
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// curtain reveal animation & play audio when clicked
+const cover = document.querySelector(".cover");
+const myAudio = document.getElementById("myaudio");
+
+cover.addEventListener("click", () => {
+    cover.classList.add("hide-cover");
+    myAudio.play();
 });
